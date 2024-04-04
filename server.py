3daf4269a53bff01d8f9a22445d9cb794c73ccd5
Stefan Weiland <stefanweiland@fastmail.com>
2024-04-04 12:26:29 +0200

    server port as optional input argument

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import http.server
 import socketserver
+from sys import argv
 
 # Define the port number to run the server on
-PORT = 8000
+PORT = int(argv[1]) if len(argv) > 1 else 8000
 
 # Set up the HTTP request handler
 Handler = http.server.SimpleHTTPRequestHandler
